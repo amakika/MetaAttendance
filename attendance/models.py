b8333e05e0a224f2c8315b30b0cd0deb5d5adbac
Yasin <asinarstanbekov51@gmail.com>
2024-10-03 01:45:47 +0300

    adding host

--- attendance/models.py
+++ attendance/models.py
@@ -62,9 +62,3 @@ class Attendance(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.date} - {self.status}"
 
-class Subject(models.Model):
-    name = models.CharField(max_length=100)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='subjects')
-
-    def __str__(self):
-        return self.name
