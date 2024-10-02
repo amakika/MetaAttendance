7b8afdaefcc9ac4d91d9256e242597a6bf2562cf
Yasin <asinarstanbekov51@gmail.com>
2024-10-03 02:13:29 +0300

    adding host

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 from .views import leaderboard 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
