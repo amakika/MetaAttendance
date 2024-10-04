9b5ef58e68cbea6717a30976f615be758c93f79f
Yasin <asinarstanbekov51@gmail.com>
2024-10-05 02:07:39 +0300

    Improving students tables

--- MetaAttendance/settings.py
+++ MetaAttendance/settings.py
@@ -113,12 +113,6 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
